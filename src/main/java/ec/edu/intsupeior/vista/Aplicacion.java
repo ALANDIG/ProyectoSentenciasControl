1a159f0febeef689c017a0edf06208fd10f72759
ALANDIG <alexandergenius@hotmail.com>
2022-01-26 22:48:32 -0500

    Landiman94 primero

---
+++ src/main/java/ec/edu/intsupeior/vista/Aplicacion.java
@@ -0,0 +1,19 @@
+
+package ec.edu.intsupeior.vista;
+
+import ec.edu.intsupeior.controlador.Pais;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        Pais pais1= new Pais("p001","Argentina");
+        Pais pais2= new Pais("p002","Brasil");
+        Pais pais3= new Pais("p003","Bolivia");
+        Pais pais4= new Pais("p004","Chile");
+
+        //System.out.println(pais1.getCodigoPais()+" - "+pais1.getNombrePais());
+        System.out.println(pais3.toString());
+
+    }
+    
+}
